19a2e5e7528853945c150aaff1aac4ed81a0e019
Willian Rodriguez <willian@amobasoftware.com>
2026-07-03 22:27:00 -0400

    app model

--- app/Offer.php
+++ app/Offer.php
@@ -3,10 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Offer extends Model
 {
-    
+    use SoftDeletes;
     protected $table = "offers";
 
     public function user(){
